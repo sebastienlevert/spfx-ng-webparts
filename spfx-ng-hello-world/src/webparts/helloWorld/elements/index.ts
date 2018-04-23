fde4650b7f071f68e6f8ba6e735c993aa1661862
Rainson12 <rene.steen@steen-ie.de>
2018-04-23 11:34:04 +0200

    Update index.ts
    update to angular 6 rc 5

--- spfx-ng-hello-world/src/webparts/helloWorld/elements/index.ts
+++ spfx-ng-hello-world/src/webparts/helloWorld/elements/index.ts
@@ -1,13 +1,9 @@
 //web components ES5 shim
 import './../../../elements/wc-shim.js';
-import { registerAsCustomElements } from '@angular/elements';
 import { platformBrowser } from '@angular/platform-browser';
 
 import { HelloWorld, HelloWorldModule } from './hello-world';
 import { HelloWorldModuleNgFactory } from './hello-world.ngfactory';
 
-registerAsCustomElements(
-  [ HelloWorld ],
-  () => platformBrowser().bootstrapModuleFactory(HelloWorldModuleNgFactory, { ngZone: 'noop' })
-).catch(err => console.log(err));
+platformBrowser().bootstrapModuleFactory(HelloWorldModuleNgFactory, { ngZone: 'noop' });
 
